1c830a3562f253e49035373dc40ee0b4437c50f1
Natanael <natanael@virmala.com>
2026-03-06 22:26:53 +0700

    fix: corrupted __init__.py closing bracket merged with import line

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -1,21 +1,21 @@
 from .extract_audio import extract_audio
 from .transcribe import transcribe_audio
-from .srt_generator import generate_srt, generate_ass
-from .translate import translate_srt, install_language_pack, get_available_languages
-from .smart_clip import find_highlights, cut_clips
-from .burn_captions import burn_captions
-from .gemini_clip import find_highlights_gemini
+from .captions import generate_captions
+from .translate import translate_segments
+from .clip import smart_clip
+from .burn import burn_captions
+
+try:
+    from .gemini_clip import gemini_clip
+except ImportError:
+    gemini_clip = None
 
 __all__ = [
     "extract_audio",
     "transcribe_audio",
-    "generate_srt",
-    "generate_ass",
-    "translate_srt",
-    "install_language_pack",
-    "get_available_languages",
-    "find_highlights",
-    "find_highlights_gemini",
-    "cut_clips",
+    "generate_captions",
+    "translate_segments",
+    "smart_clip",
+    "gemini_clip",
     "burn_captions",
 ]
