5a04f2dc92ed016c1f2070362218531be0b5ee29
Natanael <natanael@virmala.com>
2026-03-06 22:35:31 +0700

    fix: add all missing imports - install_language_pack, find_highlights_gemini

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -1,16 +1,17 @@
 from .extract_audio import extract_audio
 from .transcribe import transcribe_audio
 from .srt_generator import generate_srt, generate_ass, generate_word_highlight_ass
-from .translate import translate_segments
+from .translate import translate_segments, translate_srt, install_language_pack
 from .smart_clip import find_highlights, find_highlights_llm, cut_clips
 from .burn_captions import burn_captions
 
 try:
-    from .gemini_clip import gemini_clip
+    from .gemini_clip import gemini_clip, find_highlights_gemini
 except ImportError:
     gemini_clip = None
+    find_highlights_gemini = None
 
-# Aliases for main.py compatibility
+# Aliases for compatibility
 generate_captions = generate_srt
 smart_clip = find_highlights
 
@@ -22,8 +23,11 @@ __all__ = [
     "generate_word_highlight_ass",
     "generate_captions",
     "translate_segments",
+    "translate_srt",
+    "install_language_pack",
     "find_highlights",
     "find_highlights_llm",
+    "find_highlights_gemini",
     "cut_clips",
     "smart_clip",
     "gemini_clip",
